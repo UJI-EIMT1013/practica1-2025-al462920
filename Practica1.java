1e0fc97f6c3b63cf7e548ebbd676f4e2713ba8cd
al462920 <al462920@uji.es>
2025-09-29 18:37:07 +0200

    ejercicio2 hecho

--- Practica1.java
+++ Practica1.java
@@ -57,8 +57,70 @@ public class Practica1 {
 
     //EJERCICIO2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
-        //TODO
+        
+        // Creamos Set auxiliar que sera la UNION de los 2, para asi poder recorrerlo
+        HashSet<Integer> set_auxiliar = new HashSet<>(cuadrados);
 
+        // Hacemos la union
+        set_auxiliar.addAll(noCuadrados);
+
+        // Guardamos si el 1 estaba en los dos sets originales, CASO RARO PORQUE EN VERDAD EL CUADRADO DE 1 ES 1
+        boolean unoEnAmbos = cuadrados.contains(1) && noCuadrados.contains(1);
+        boolean unoPresente = set_auxiliar.contains(1);
+
+        // Borramos los Set para dejarlos vacios
+        cuadrados.clear();
+        noCuadrados.clear();
+
+        // Bucle que para cada numero calcula su cuadrado y pregunta si esta en el set_auxiliar
+        for (Integer n: set_auxiliar) {
+
+            // Evitamos números negativos y el cero
+            if (n <= 0) continue;
+
+            int raiz = (int) Math.sqrt(n);
+
+            // comprobamos si es un cuadrado perfecto y su raíz está en el set
+            if (raiz * raiz == n && set_auxiliar.contains(raiz)) {
+                cuadrados.add(n);   // metemos el número (el cuadrado)
+            } else {
+                noCuadrados.add(n);
+            }
+
+
+
+            // Calculamos el cuadrado
+            //int cuadrado = potencial_cuadrado * potencial_cuadrado;
+
+            // Miramos si esta en el set_auxiliar
+            //if (set_auxiliar.contains(cuadrado)) {
+
+                // Esta en set_auxiliar, por tanto metemos el cuadrado a el set cuadrado
+                //cuadrados.add(cuadrado);
+                
+            //}
+            //else {
+
+                // No esta en set_auxiliar por lo tanto lo metemos a noCuadrados
+                //noCuadrados.add(potencial_cuadrado);
+
+            //}
+        }
+
+        // Tratamiento especial del 1
+        if (unoPresente) {
+            if (unoEnAmbos) {
+                // Lo pasamos a cuadrados
+                cuadrados.add(1);
+                noCuadrados.remove(1);
+            }  
+            else {
+            // Lo pasamos a noCuadrados
+            cuadrados.remove(1);
+            noCuadrados.add(1);
+            }
+        }
+        
     }
 
     //EJERCICIO 3
